4cdfc638346739b4d494714acbb3e5614ac75fb5
Duerxiyaa <2167183692@qq.com>
2021-06-11 11:19:22 +0800

    '数据库配置';

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'itheima',
+        'PASSWORD': '123456',
+        'NAME': 'itheima_blog'
+    },
 }
 
 
